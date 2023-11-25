655cee1ddd0d352c412006a54a2e68d9cff158fc
QosimjonEliev <128938129+QosimjonEliev@users.noreply.github.com>
2023-11-25 18:20:38 -0500

    Add files via upload

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,18 +1,6 @@
 import { baseUrl } from "./constants";
 import { checkResponse } from "./utils";
 
-//sign in
-export const postSignIn = ({ email, password }) => {
-  return fetch(`${baseUrl}/signin`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ email, password }),
-  }).then(checkResponse);
-};
-
 //sign up
 export const postSignup = ({ email, password, name, avatar }) => {
   return fetch(`${baseUrl}/signup`, {
@@ -22,6 +10,18 @@ export const postSignup = ({ email, password, name, avatar }) => {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password, name, avatar }),
+  }).then(checkResponse);
+};
+
+//sign in
+export const postSignIn = ({ email, password }) => {
+  return fetch(`${baseUrl}/signin`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
   }).then(checkResponse);
 };
 
